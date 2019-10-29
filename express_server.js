8905b9d0523cbdbc0e9d8ded555d19e3d234faea
HermSidhu <h-sidhu@hotmail.com>
2019-10-29 18:43:54 +0000

    implemented POST Request

--- express_server.js
+++ express_server.js
@@ -1,17 +1,33 @@
+const bodyParser = require("body-parser");
 const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.set('view engine', 'ejs')
+function generateRandomString() {
+  
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.goole.com"
+  "9sm5xK": "http://www.google.com"
 };
+
+app.set('view engine', 'ejs')
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get('/urls/:shortURL', (req, res) => {
