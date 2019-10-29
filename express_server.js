76d0f823748b9f7318c44f2a6b2c7230302bf1e5
HermSidhu <h-sidhu@hotmail.com>
2019-10-29 00:43:00 +0000

    added req.params

--- express_server.js
+++ express_server.js
@@ -14,6 +14,12 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+app.get('/urls/:shortURL', (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  console.log(templateVars)
+  res.render("urls_show", templateVars);
+});
+
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
