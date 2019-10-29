6988d944d73d7d114268f97aa2c98846ba1259ff
HermSidhu <h-sidhu@hotmail.com>
2019-10-29 19:49:24 +0000

    long URL gets short URL and short URL opens long URL

--- express_server.js
+++ express_server.js
@@ -3,9 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-function generateRandomString() {
-  
-}
+function generateRandomString(length) {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+};
+
+app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -14,12 +22,13 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.set('view engine', 'ejs')
 
 app.post('/urls', (req, res) => {
+  let id = generateRandomString(6)
   console.log(req.body);
-  res.send("Ok");
-})
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`)
+});
 
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
@@ -31,10 +40,16 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  console.log(templateVars)
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  console.log(req.params)
   res.render("urls_show", templateVars);
 });
+
+app.get('/u/:shortURL', (req, res) => {
+  let shortcut = req.params.shortURL;
+  res.redirect(urlDatabase[shortcut])
+  console.log(shortcut + urlDatabase[shortcut]);
+ });
 
 app.get('/', (req, res) => {
   res.send("Hello!");
